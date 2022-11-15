dd7fc239ef384bba02919861456e92d824afb18f
Berniko <DivineEternity7@gmail.com>
2022-11-15 12:43:39 +0400

    updatess

--- server.js
+++ server.js
@@ -488,7 +488,7 @@ mongoose
   .then(() => console.log(`Database connected`))
   .catch((error) => console.log(`Database error: ${error}`));
 
-server.listen(process.env.PORT || 5000, (error) => {
+server.listen(process.env.PORT || 80, (error) => {
   error && console.log(`Server error: ${error}`);
   console.log(`Server connected`);
 });
