d368943b149c1db1160de52d57928e49b1b8e83c
Berniko <DivineEternity7@gmail.com>
2023-01-10 16:48:41 +0400

    update2

--- server.js
+++ server.js
@@ -478,7 +478,7 @@ io.on("connection", async (socket) => {
 
 app.use((req, res, next) => {
   if (req.protocol === "http") {
-    res.redirect(301, `https://${req.headers.host}${req.url}`);
+    return res.redirect(301, `https://${req.headers.host}${req.url}`);
   }
   next();
 });
